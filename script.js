f68a8d25b92a8d7bd4609a23fffde04fa232d5ce
Zameer Haque <haque864@gmail.com>
2017-09-21 23:32:25 +0530

    add Speech bubble, and button text changed to send

--- script.js
+++ script.js
@@ -9,29 +9,26 @@ formid.addEventListener('submit',function(e){
 	
 	var textarea = document.getElementById('textarea');
 	var text = textarea.value;
-	var newParagraph = document.createElement('p');
+	var newP = document.createElement('p');
 	var myTextNode = document.createTextNode(text);
-	newParagraph.appendChild(myTextNode);
+	newP.appendChild(myTextNode);
 
-	var newMyMsgDiv = document.createElement('div');
-	newMyMsgDiv.setAttribute('class','mynewmessage');
-	newMyMsgDiv.appendChild(newParagraph);
-	messages.appendChild(newMyMsgDiv);
+	var newDiv = document.createElement('div');
+	newDiv.setAttribute('class','mynewmessage');
+	newDiv.appendChild(newP);
+	messages.appendChild(newDiv);
 	
 	textarea.value = '';
 	
 	setTimeout(function(){
-		var newParagraph = document.createElement('p');
+		var newP = document.createElement('p');
 
-		var newSusiMsgDiv = document.createElement('div');
-		var newMsgDiv = document.createElement('div');
-		newMsgDiv.setAttribute('class','newmessage');
-		var susiTextNode = document.createTextNode("hi from susi");
-		newSusiMsgDiv.setAttribute('class','susinewmessage');
-		newParagraph.appendChild(susiTextNode);
-		newSusiMsgDiv.appendChild(newParagraph);
-		newMsgDiv.appendChild(newSusiMsgDiv);
-		messages.appendChild(newMsgDiv);
+		var newDiv = document.createElement('div');
+		var susiTextNode = document.createTextNode("Hello world! How you like my new look ? :D");
+		newDiv.setAttribute('class','susinewmessage');
+		newP.appendChild(susiTextNode);
+		newDiv.appendChild(newP);
+		messages.appendChild(newDiv);
 		messages.scrollTop = messages.scrollHeight;
 	},2000);
 	messages.scrollTop = messages.scrollHeight;
