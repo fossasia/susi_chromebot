d33362809badd1fed943c9b8609cef6ceeac60a0
Mohit Sharma <mohit1532.cse16@chitkara.edu.in>
2017-09-22 01:16:58 +0530

    Updated submit function

--- script.js
+++ script.js
@@ -31,3 +31,28 @@ textarea.onkeyup = function(e) {
     messages.scrollTop = messages.scrollHeight;
   }
 };
+
+formid.addEventListener('submit',function(e){
+    e.preventDefault();
+    var textarea = document.getElementById('textarea');
+    var text = textarea.value;
+    var newP = document.createElement('p');
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'mynewmessage');
+    var myTextNode = document.createTextNode(text);
+    newP.appendChild(myTextNode);
+    newDiv.appendChild(newP);
+    messages.appendChild(newDiv);
+    textarea.value = '';
+    setTimeout(function() {
+      var newP = document.createElement('p');
+      var newDiv = document.createElement('div');
+      var susiTextNode = document.createTextNode("hi from susi");
+      newDiv.setAttribute('class', 'susinewmessage');
+      newP.appendChild(susiTextNode);
+      newDiv.appendChild(newP);
+      messages.appendChild(newDiv);
+      messages.scrollTop = messages.scrollHeight;
+    }, 2000);
+    messages.scrollTop = messages.scrollHeight;
+  });
