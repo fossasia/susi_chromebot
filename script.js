a914b616f140230e2cec0535e976f4ca89030259
Zameer Haque <haque864@gmail.com>
2017-09-21 17:35:09 +0530

    Modified colors pushed messages to left and right

--- script.js
+++ script.js
@@ -5,26 +5,34 @@ var messages = document.getElementById("messages");
 
 
 formid.addEventListener('submit',function(e){
-    e.preventDefault();
-    var textarea = document.getElementById('textarea');
-    var text = textarea.value;
-    var newP = document.createElement('p');
-    var newDiv = document.createElement('div');
-    newDiv.setAttribute('class','mynewmessage');
-    var myTextNode = document.createTextNode(text);
-    newP.appendChild(myTextNode);
-    newDiv.appendChild(newP);
-    messages.appendChild(newDiv);
-    textarea.value = '';
-    setTimeout(function(){
-        var newP = document.createElement('p');
-        var newDiv = document.createElement('div');
-        var susiTextNode = document.createTextNode("hi from susi");
-        newDiv.setAttribute('class','susinewmessage');
-        newP.appendChild(susiTextNode);
-        newDiv.appendChild(newP);
-        messages.appendChild(newDiv);
-        messages.scrollTop = messages.scrollHeight;
-    },2000);
-    messages.scrollTop = messages.scrollHeight;
+	e.preventDefault();
+	
+	var textarea = document.getElementById('textarea');
+	var text = textarea.value;
+	var newParagraph = document.createElement('p');
+	var myTextNode = document.createTextNode(text);
+	newParagraph.appendChild(myTextNode);
+
+	var newMyMsgDiv = document.createElement('div');
+	newMyMsgDiv.setAttribute('class','mynewmessage is-pulled-right is-clearfix');
+	newMyMsgDiv.appendChild(newParagraph);
+	messages.appendChild(newMyMsgDiv);
+	
+	textarea.value = '';
+	
+	setTimeout(function(){
+		var newParagraph = document.createElement('p');
+
+		var newSusiMsgDiv = document.createElement('div');
+		var newMsgDiv = document.createElement('div');
+		newMsgDiv.setAttribute('class','newmessage');
+		var susiTextNode = document.createTextNode("hi from susi");
+		newSusiMsgDiv.setAttribute('class','susinewmessage is-pulled-left is-clearfix');
+		newParagraph.appendChild(susiTextNode);
+		newSusiMsgDiv.appendChild(newParagraph);
+		newMsgDiv.appendChild(newSusiMsgDiv);
+		messages.appendChild(newMsgDiv);
+		messages.scrollTop = messages.scrollHeight;
+	},2000);
+	messages.scrollTop = messages.scrollHeight;
 });
