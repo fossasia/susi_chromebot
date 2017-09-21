07b10421060e3f78066293f00d137de4b47ec61d
Mohit Sharma <mohit1532.cse16@chitkara.edu.in>
2017-09-21 20:22:56 +0530

    Fixed action for return key

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ var message = document.getElementById('message');
 var messages = document.getElementById('messages');
 var formid = document.getElementById('formid');
 var messages = document.getElementById("messages");
+var textarea = document.getElementById('textarea');
 
 
-formid.addEventListener('submit',function(e){
+textarea.onkeyup = function(e) {
+  if (e.which == 13 && !e.shiftKey) {
     e.preventDefault();
     var textarea = document.getElementById('textarea');
     var text = textarea.value;
     var newP = document.createElement('p');
     var newDiv = document.createElement('div');
-    newDiv.setAttribute('class','mynewmessage');
+    newDiv.setAttribute('class', 'mynewmessage');
     var myTextNode = document.createTextNode(text);
     newP.appendChild(myTextNode);
     newDiv.appendChild(newP);
     messages.appendChild(newDiv);
     textarea.value = '';
-    setTimeout(function(){
-        var newP = document.createElement('p');
-        var newDiv = document.createElement('div');
-        var susiTextNode = document.createTextNode("hi from susi");
-        newDiv.setAttribute('class','susinewmessage');
-        newP.appendChild(susiTextNode);
-        newDiv.appendChild(newP);
-        messages.appendChild(newDiv);
-        messages.scrollTop = messages.scrollHeight;
-    },2000);
+    setTimeout(function() {
+      var newP = document.createElement('p');
+      var newDiv = document.createElement('div');
+      var susiTextNode = document.createTextNode("hi from susi");
+      newDiv.setAttribute('class', 'susinewmessage');
+      newP.appendChild(susiTextNode);
+      newDiv.appendChild(newP);
+      messages.appendChild(newDiv);
+      messages.scrollTop = messages.scrollHeight;
+    }, 2000);
     messages.scrollTop = messages.scrollHeight;
-});
+  }
+};
