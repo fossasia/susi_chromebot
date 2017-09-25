c897132d0c8b0f5cca5c757e93711d0bef591c0c
Mohit Sharma <mohit1532.cse16@chitkara.edu.in>
2017-09-25 22:59:59 +0530

    Integrated Travis CI

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/*global module*/
+module.exports = function (grunt) {
+  'use strict';
+
+  var gruntConfig = {};
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  gruntConfig.jshint = {
+      options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
+          indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
+          quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
+          maxparams: 3, maxdepth: 2, maxstatements: 50, jquery: true, browser: true, devel: true},
+      all: [
+          'Gruntfile.js',
+          'src/*.js'
+      ]
+  };
+  grunt.initConfig(gruntConfig);
+  grunt.registerTask('travis', 'jshint');
+
+};
