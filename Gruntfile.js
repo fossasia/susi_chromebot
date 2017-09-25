5d7e758dd6cb604487212671497891d163346f36
Mohit Sharma <mohit1532.cse16@chitkara.edu.in>
2017-09-25 23:41:52 +0530

    Fixed JSHint Errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,8 @@ module.exports = function (grunt) {
   gruntConfig.jshint = {
       options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
           indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
-          quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
-          maxparams: 3, maxdepth: 2, maxstatements: 50, jquery: true, browser: true, devel: true},
+          quotmark: true, regexp: true, undef: true, unused: true, trailing: true,
+          maxparams: 3, maxdepth: 2, maxstatements: 50, jquery: true, browser: true, devel: true, moz:true, es5: true, strict: false},
       all: [
           'Gruntfile.js',
           'src/*.js'
