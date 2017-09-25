e52e87981f43ab8cd4a8e0105610debb0b0d4a08
Zameer Haque <haque864@gmail.com>
2017-09-25 13:14:41 +0530

    add composesusi message for errors

--- src/script.js
+++ src/script.js
@@ -4,84 +4,64 @@ var messages = document.getElementById("messages");
 var formid = document.getElementById("formid");
 var textarea = document.getElementById("textarea");
 
+function composeResponse(data){
+    var link = data["answers"][0]["data"][0]["link"];
+    var image1 = data["answers"][0]["data"][0]["answer"];
+    var image2 = data["answers"][0]["data"][0]["webformatURL"];
+    var image3 = data["answers"][0]["data"][0]["image"];
+    var answer = data["answers"][0]["data"][0]["answer"];
+    var image;
+    try {
+        if (image2.startsWith("https")) {
+            image=image2;
+            answer="";
+        }
+    }catch(err)
+    {}
+    try {
+        if (image3.startsWith("https")) {
+            image=image3;
+        }
+    }
+    catch(err)
+    {}
+    try {
+        if (image1.startsWith("https")) {
+            image=image1;
+            answer="";	
+        }
+    }
+    catch(err)
+    {}
+    return {
+        error: false,
+        link,
+        image,
+        answer
+    };
+}
+
 function getResponse(query) {
     $.ajax({
         dataType: "jsonp",
         type: "GET",
         url: "https://api.susi.ai/susi/chat.json?timezoneOffset=-300&q=" + query,
         error: function(xhr,textStatus,errorThrown) {
-            var newP = document.createElement("p");
-            var newDiv = messages.childNodes[messages.childElementCount];
             loading(false);
-            newDiv.setAttribute("class", "susinewmessage");
-            var myTextNode = document.createTextNode("Sorry! request could not be made.");
-            newP.appendChild(myTextNode);
-            newDiv.appendChild(newP);
-            messages.appendChild(newDiv);        
+            var response = {
+                error: true,
+                errorText: "Sorry! request could not be made"
+            };
+            composeSusiMessage(response);        
         },
         success: function (data) {
             var recQuery = data["answers"][0]["data"][0]["query"];
             if (query !== recQuery) {
                 return getResponse(query);
             }
-            var link = data["answers"][0]["data"][0]["link"];
-            var image1 = data["answers"][0]["data"][0]["answer"];
-            var image2 = data["answers"][0]["data"][0]["webformatURL"];
-            var image3 = data["answers"][0]["data"][0]["image"];
-            var answer = data["answers"][0]["data"][0]["answer"];
-            console.log(image1);
-            console.log(image2);
-    		console.log(image3);
-            try {
-            if (image2.startsWith("https"))
-            {
-                var image=image2;
-                answer="";
-            }
-            } catch(err)
-            {}
-            try{
-            
-            if (image3.startsWith("https"))
-            {
-            	var image=image3;
-        	}
-            }
-            catch(err)
-            {}
-            try {
-            if (image1.startsWith("https"))
-            {
-                var image=image1;
-                answer="";	
-            }
-            }
-            catch(err)
-            {}
+            var response = composeResponse(data);
             loading(false);
-        	console.log(image);
-            var newDiv = messages.childNodes[messages.childElementCount];
-            var newP = document.createElement("p");
-            newP.appendChild(document.createTextNode(answer));
-            newDiv.appendChild(newP);
-            if (link) {
-                var newA = document.createElement("a");
-                newA.appendChild(document.createTextNode(link));
-                newA.setAttribute("href", link);
-                newA.setAttribute("target", "_blank");
-                newDiv.appendChild(document.createElement("br"));
-                newDiv.appendChild(newA);
-            }
-            if (image) {
-                var newImg = document.createElement("img");
-                newDiv.appendChild(document.createElement("br"));
-                newImg.setAttribute("src", image);
-                newImg.setAttribute("class", "susi-img");
-                newDiv.appendChild(newImg);
-            }   
-            newDiv.setAttribute("class", "susinewmessage");
-            messages.appendChild(newDiv);
-            messages.scrollTop = messages.scrollHeight;
+            composeSusiMessage(response);
         }
     });
 }
@@ -102,12 +82,7 @@ function loading(condition=true){
     }
 }
 
-function submitForm() {
-    var text = textarea.value;
-    text = text.trim();
-    if (text === "") {
-        return console.log("Empty Query!");
-    }
+function composeMyMessage(text) {
     var newP = document.createElement("p");
     var newDiv = document.createElement("div");
     newDiv.setAttribute("class", "mynewmessage");
@@ -116,20 +91,71 @@ function submitForm() {
     newDiv.appendChild(newP);
     messages.appendChild(newDiv);
     textarea.value = "";
+    messages.scrollTop = messages.scrollHeight;
+}
+
+function composeLink(link) {
+    var newA = document.createElement("a");
+    newA.appendChild(document.createTextNode(link));
+    newA.setAttribute("href", link);
+    newA.setAttribute("target", "_blank");
+    return newA;
+}
+
+function composeImage(image) {
+    var newImg = document.createElement("img");
+    newImg.setAttribute("src", image);
+    newImg.setAttribute("class", "susi-img"); 
+    return newImg;
+}
+
+function composeSusiMessage(response) {
+    var newP = document.createElement("p");
+    var newDiv =  messages.childNodes[messages.childElementCount];
+    newDiv.setAttribute("class", "susinewmessage");
+    var susiTextNode;
+    if(response.error===true){
+        susiTextNode = document.createTextNode(response.errorText);
+        newP.appendChild(susiTextNode);
+        newDiv.appendChild(newP);
+    }
+    else {
+            susiTextNode = document.createTextNode(response.answer);
+            newP.appendChild(susiTextNode);
+            newDiv.appendChild(newP);
+            if (response.link) {
+                var newA = composeLink(response.link);
+                newDiv.appendChild(document.createElement("br"));
+                newDiv.appendChild(newA);
+            }
+            if (response.image) {
+                var newImg = composeImage(response.image);
+                newDiv.appendChild(document.createElement("br"));
+                newDiv.appendChild(newImg);
+            }   
+    }
+    messages.appendChild(newDiv);
+    messages.scrollTop = messages.scrollHeight;
+}
+
+function submitForm() {
+    var text = textarea.value;
+    text = text.trim();
+    if (text === "") {
+        return console.log("Empty Query!");
+    }
+    composeMyMessage(text);
     loading();
     if(window.navigator.onLine)
         getResponse(text);
     else {
         loading(false);
-        var newP = document.createElement("p");
-        var newDiv =  messages.childNodes[messages.childElementCount];
-        newDiv.setAttribute("class", "susinewmessage");
-        var myTextNode = document.createTextNode("Not connected to Internet!");
-        newP.appendChild(myTextNode);
-        newDiv.appendChild(newP);
-        messages.appendChild(newDiv);
+        var response = {
+            error: true,
+            errorText: "Not connected to Internet!"
+        };
+        composeSusiMessage(response);
     }
-    messages.scrollTop = messages.scrollHeight;
 }
 
 textarea.onkeyup = function (e) {
