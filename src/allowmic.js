c4408f126e5bdcaa08d02a11d25ba19498c13c5a
Nitesh Kumar <niteshkumarniranjan@gmail.com>
2017-12-05 22:07:55 +0530

    Fixed UI & UX

--- src/allowmic.js
+++ src/allowmic.js
@@ -1,7 +1,24 @@
-navigator.webkitGetUserMedia({
-    audio: true
-}, function(stream) {
-    stream.stop();
-}, function () {
-    console.log('no access');
-});     
+var micAccess = document.getElementById("micAccess");
+var backgroundChange = document.getElementById("backgroundChange");
+var backUrl = document.getElementById("backUrl");
+var theValue;
+micAccess.addEventListener("click", () => {
+    navigator.webkitGetUserMedia({
+        audio: true
+    }, function (stream) {
+        stream.stop();
+    }, function () {
+        console.log("no access");
+    });
+});
+
+backgroundChange.addEventListener("submit", (e) => {
+    e.preventDefault();
+    theValue = backUrl.value;
+    if (!theValue) {
+        alert("Error: No value specified");
+    } else {
+        localStorage.setItem("theValue", theValue);
+        alert("Successfully stored");
+    }
+});
