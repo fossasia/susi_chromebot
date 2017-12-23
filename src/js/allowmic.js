b47bab61fa774c6d0141e7cceeffbdc8bb0327e1
Mohit Sharma <msvbs98@gmail.com>
2017-12-23 17:38:28 +0530

    Remove all console errors
    Removed Errors

--- src/js/allowmic.js
+++ src/js/allowmic.js
@@ -5,37 +5,37 @@ var preDefThemes = document.getElementById("preDefThemes");
 var theme;
 var theValue;
 
+if(micAccess) {
+    micAccess.addEventListener("click", () => {
+        navigator.webkitGetUserMedia({
+            audio: true
+        }, function (stream) {
+            stream.stop();
+        }, function () {
+            console.log("no access");
+        });
+    });
+}
+if(preDefThemes) {
 preDefThemes.addEventListener("click",(e) => {
     if(e.target!==e.currentTarget){
-    theme= e.target.id;
-    console.log(theme);
+        theme= e.target.id;
+        console.log(theme);
     }
     
     e.stopPropagation;
-});
-
-submit.addEventListener("click",()=>{
-    localStorage.setItem("theme",theme);
-    console.log("success");
-    alert("success");
-});
-micAccess.addEventListener("click", () => {
-    navigator.webkitGetUserMedia({
-        audio: true
-    }, function (stream) {
-        stream.stop();
-    }, function () {
-        console.log("no access");
     });
-});
+}
 
-backgroundChange.addEventListener("submit", (e) => {
-    e.preventDefault();
-    theValue = backUrl.value;
-    if (!theValue) {
-        alert("Error: No value specified");
-    } else {
-        localStorage.setItem("theValue", theValue);
-        alert("Successfully stored");
-    }
-});
+if(backgroundChange) {
+    backgroundChange.addEventListener("submit", (e) => {
+        e.preventDefault();
+        theValue = backUrl.value;
+        if (!theValue) {
+            alert("Error: No value specified");
+        } else {
+            localStorage.setItem("theValue", theValue);
+            alert("Successfully stored");
+        }
+    });
+}
