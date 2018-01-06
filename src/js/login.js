cc0f9f26ba1fe9352f6d606153be64f2a5be14bb
Harshit  Luthra <love.coolsachin@gmail.com>
2018-01-07 01:23:06 +0530

    Add theme sync through login (#264)

--- src/js/login.js
+++ src/js/login.js
@@ -8,122 +8,141 @@ var loggedInBlock = document.getElementById("loggedin");
 var accessToken = "";
 var time = "";
 var BASE_URL = "https://api.susi.ai";
-var checkLogin ;
-
-window.onload = function(){
-	chrome.storage.sync.get("loggedUser",function(userDetails){
-		if(userDetails.loggedUser.email){
-			showLoggedInBlock(true);
-		}
-		else{
-			showLoggedInBlock(false);
-		}
-	});
-
-};
-
-
-window.onload = function(){
-	chrome.storage.sync.get("loggedUser",function(userDetails){
-		if(userDetails.loggedUser.email){
-			showLoggedInBlock(true);
-		}
-		else{
-			showLoggedInBlock(false);
-		}
-	});
-
-};
-
-function showLoggedInBlock(show){
-	if(show){
-		noLoggedInBlock.style.display="none";
-		loggedInBlock.style.display="block";
-		document.getElementById("passwordchange").value="";
-		document.getElementById("passwordnewconfirm").value = "";
-		document.getElementById("passwordnew").value = "";
-	}
-	else{
-		noLoggedInBlock.style.display="block";
-		loggedInBlock.style.display="none";
-		document.getElementById("username").value = "";
-		document.getElementById("password").value = "";
-	}
-}
-
-loginForm.addEventListener("submit", function login(event){
-	event.preventDefault();
-	var email=document.getElementById("username").value;
-	var password=document.getElementById("password").value;
-	if(!email){
-		alert("Email field cannot be empty");
-		return;
-	}
-	else if(!password){
-		alert("Password field cannot be empty");
-		return;
-	}
-	$("#loginbutton").button("loading");
-	var loginEndPoint = BASE_URL+"/aaa/login.json?type=access-token&login="+ encodeURIComponent(email)+ "&password="+ encodeURIComponent(password);
-	$.ajax({
-		url: loginEndPoint,
-		dataType: "jsonp",
-		jsonpCallback: "p",
-		jsonp: "callback",
-		crossDomain: true,
-		success: function (response) {
-			if(response.accepted){
-
-				accessToken = response.accessToken;
-
-				checkLogin = "true";
-				localStorage.setItem("checkLogin",checkLogin);
-
-				chrome.storage.sync.set({
-					loggedUser:{
-						email:email,
-						accessToken: accessToken
-					}
-				});
-
-				time = response.validSeconds;
-				loginButton.innerHTML="Login";
-				$("#loginbutton").button("reset");
-				alert(response.message);
-				showLoggedInBlock(true);
-
-			}
-			else {
-				$("#loginbutton").button("reset");
-				alert("Login Failed. Try Again");
-			}
-		},
-		error: function ( jqXHR) {
-			loginButton.innerHTML="Login";
-			var msg = "";
-			var jsonValue =  jqXHR.status;
-			if (jsonValue === 404) {
-				msg = "Login Failed. Try Again";
-			}
-			else {
-				msg = "Some error occurred. Try Again";
-			}
-			if (status === "timeout") {
-				msg = "Please check your internet connection";
-			}
-			$("#loginbutton").button("reset");
-			alert(msg);
-		}
-	});
+var checkLogin;
+var defaultThemeValue = "";
+chrome.storage.sync.get("darktheme", (obj) => {
+    if (obj.darktheme === true) {
+        defaultThemeValue = "dark";
+    } else {
+        defaultThemeValue = "light";
+    }
 });
+window.onload = function() {
+    chrome.storage.sync.get("loggedUser", function(userDetails) {
+        if (userDetails.loggedUser.email) {
+            showLoggedInBlock(true);
+        } else {
+            showLoggedInBlock(false);
+        }
+    });
+};
+window.onload = function() {
+    chrome.storage.sync.get("loggedUser", function(userDetails) {
+        if (userDetails.loggedUser.email) {
+            showLoggedInBlock(true);
+        } else {
+            showLoggedInBlock(false);
+        }
+    });
+};
 
-logoutButton.addEventListener("click", function logout(e){
+function showLoggedInBlock(show) {
+    if (show) {
+        noLoggedInBlock.style.display = "none";
+        loggedInBlock.style.display = "block";
+        document.getElementById("passwordchange").value = "";
+        document.getElementById("passwordnewconfirm").value = "";
+        document.getElementById("passwordnew").value = "";
+    } else {
+        noLoggedInBlock.style.display = "block";
+        loggedInBlock.style.display = "none";
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+    }
+}
+loginForm.addEventListener("submit", function login(event) {
+    event.preventDefault();
+    var email = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (!email) {
+        alert("Email field cannot be empty");
+        return;
+    } else if (!password) {
+        alert("Password field cannot be empty");
+        return;
+    }
+    $("#loginbutton").button("loading");
+    var loginEndPoint = BASE_URL + "/aaa/login.json?type=access-token&login=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
+    $.ajax({ // Checks if the user has pevious UserSettings to update or not
+        url: loginEndPoint,
+        dataType: "jsonp",
+        jsonpCallback: "p",
+        jsonp: "callback",
+        crossDomain: true,
+        success: function(response) {
+            if (response.accepted) {
+                accessToken = response.access_token;
+                var listUserSettings = BASE_URL + "/aaa/listUserSettings.json?access_token=" + accessToken;
+                checkLogin = "true";
+                localStorage.setItem("checkLogin", checkLogin);
+                chrome.storage.sync.set({
+                    loggedUser: {
+                        email: email,
+                        accessToken: accessToken
+                    }
+                }); // Ends here ~~~~~!
+                time = response.validSeconds;
+                loginButton.innerHTML = "Login";
+                $("#loginbutton").button("reset");
+                alert(response.message);
+                $.ajax({
+                    url: listUserSettings,
+                    dataType: "jsonp",
+                    jsonpCallback: "p",
+                    jsonp: "callback",
+                    crossDomain: "true",
+                    success: function(response) {
+                        if (response.accepted) {
+                            if (response.settings.theme != null) {
+                                var userThemeValue = response.settings.theme;
+                                if (defaultThemeValue !== userThemeValue) {
+                                    defaultThemeValue = userThemeValue;
+                                    if (defaultThemeValue === "dark") {
+                                        chrome.storage.sync.set({
+                                            "darktheme": true
+                                        }, () => {});
+                                    } else {
+                                        chrome.storage.sync.set({
+                                            "darktheme": false
+                                        }, () => {});
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                });
+
+                showLoggedInBlock(true);
+            } else {
+                $("#loginbutton").button("reset");
+                alert("Login Failed. Try Again");
+            }
+        },
+        error: function(jqXHR) {
+            loginButton.innerHTML = "Login";
+            var msg = "";
+            var jsonValue = jqXHR.status;
+            if (jsonValue === 404) {
+                msg = "Login Failed. Try Again";
+            } else {
+                msg = "Some error occurred. Try Again";
+            }
+            if (status === "timeout") {
+                msg = "Please check your internet connection";
+            }
+            $("#loginbutton").button("reset");
+            alert(msg);
+        }
+    });
+});
+logoutButton.addEventListener("click", function logout(e) {
     e.preventDefault();
     window.location.reload();
-    checkLogin="false";
-    localStorage.setItem("checkLogin",checkLogin);
-	chrome.storage.sync.remove("messagesHistory");
-	chrome.storage.sync.remove("loggedUser");
+    checkLogin = "false";
+    localStorage.setItem("checkLogin", checkLogin);
+    chrome.storage.sync.remove("messagesHistory");
+    chrome.storage.sync.remove("loggedUser");
 });
 
 document.addEventListener("DOMContentLoaded", showLoggedInBlock(false));
