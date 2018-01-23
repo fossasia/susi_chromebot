4ec896d4dd910f741593b92a9e321fa6200198b6
Arnav Mandal <arnav.mandal1234@gmail.com>
2018-01-23 14:45:46 +0530

    Susi now has a female voice

--- src/js/allowmic.js
+++ src/js/allowmic.js
@@ -3,14 +3,18 @@ var backgroundChange = document.getElementById("backgroundChange");
 var backUrl = document.getElementById("backUrl");
 var preDefThemes = document.getElementById("preDefThemes");
 var msgPaneThemes = document.getElementById("msgPaneThemes");
+var getVoice = document.getElementById("getVoice");
 var submit1 = document.getElementById("submit1");
 var submit2 = document.getElementById("submit2");
+var submit3 = document.getElementById("submit3");
 var theme;
 var msgTheme;
 var theValue;
 var warning = document.getElementById("warning");
 var customBackground = document.getElementById("customBackground");
 var checkLogin = localStorage.getItem("checkLogin");
+var getVoice = document.getElementById("getVoice");
+var voice;
 
 
 window.onload = () => {
@@ -40,7 +44,7 @@ preDefThemes.addEventListener("click",(e) => {
         theme= e.target.id;
         console.log(theme);
     }
-    
+
     e.stopPropagation;
     });
 }
@@ -63,7 +67,7 @@ preDefThemes.addEventListener("click",(e) => {
         theme= e.target.id;
         console.log(theme);
     }
-    
+
     e.stopPropagation;
     });
 }
@@ -74,25 +78,40 @@ if(msgPaneThemes) {
          msgTheme= e.target.id;
        console.log(msgTheme);
     }
- 
+
       e.stopPropagation;
       });
   }
+
+if(getVoice) {
+  getVoice.addEventListener("click",(e) => {
+     if(e.target!==e.currentTarget){
+         voice= e.target.id;
+    }
+
+     e.stopPropagation;
+     });
+}
 
 if(submit1) {
 			submit1.addEventListener("click",()=>{
 			localStorage.setItem("theme",theme);
 			alert("success");
       });
-  }
+}
 
 if(submit2) {
 			submit2.addEventListener("click",()=>{
 			localStorage.setItem("msgTheme",msgTheme);
 			alert("success");
-     });
- }
- 
+      });
+}
+if(submit3) {
+ 			submit3.addEventListener("click",()=>{
+ 			localStorage.setItem("voice",voice);
+ 			alert("success");
+      });
+}
 if(backgroundChange) {
     backgroundChange.addEventListener("submit", (e) => {
         e.preventDefault();
