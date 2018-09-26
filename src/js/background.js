4fc50d20ddda97b0abe34b24f7e7bb191d67a433
Shubham Gupta <shubhamg@outlook.com>
2018-09-27 01:35:36 +0530

    Open Chrome Extension to Left as window #344

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,7 @@
 /* jshint ignore:start */
+let windows = {};
+let lastPosition = null;
+
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript({
         file: 'script.js'
@@ -24,10 +27,16 @@ var goToSkills = chrome.contextMenus.create({
 	id:"goToSkillsSusi"
 });
 
+var openWindow = chrome.contextMenus.create({
+    id: 'susi-to-left',
+    title: 'Open as window',
+    contexts: ['all']
+  });
+
 // perform action on clicking a context menu
 chrome.contextMenus.onClicked.addListener(function(info,tab){
 	var menuId = info.menuItemId;
-	var query = info.selectionText;
+    var query = info.selectionText;
 	if(menuId==="askSusi"){
 		chrome.storage.local.set({
 			"askSusiQuery":query
@@ -39,11 +48,43 @@ chrome.contextMenus.onClicked.addListener(function(info,tab){
     	chrome.tabs.create({ url: chatURL });
 	}
 
-	else{
+	else if(menuId === "goToSkills"){
 		var skillURL = "https://skills.susi.ai";
     	chrome.tabs.create({ url: skillURL });
 	}
-
+    else{
+        openDevToolsWindow('susi-to-left')
+    }
 });
+
+function openDevToolsWindow(position) {
+    function popWindow(action, url, customOptions) {
+      function focusIfExist(callback) {
+          debugger;
+          callback();
+          lastPosition = position;
+      }
+  
+      focusIfExist(() => {
+        let options = {
+          type: 'popup',
+          ...customOptions
+        };
+        if (action === 'open') {
+          options.url = chrome.extension.getURL(url + '#' + position.substr(position.indexOf('-') + 1));
+          chrome.windows.create(options, (win) => {
+            windows[position] = win.id;
+            if (navigator.userAgent.indexOf('Firefox') !== -1) {
+              chrome.windows.update(win.id, { focused: true, ...customOptions });
+            }
+          });
+        }
+      });
+    }
+  
+    let params = { left: 0, top: 0, width: 350, height: 430 };
+    let url = 'popup.html';
+    popWindow('open', url, params);
+  }
 
 /* jshint ignore:end */
