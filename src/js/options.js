d92f0b64afa3de52c8b97359c6c9333bac313da0
sk9331657 <sk9331657@gmail.com>
2018-10-22 19:54:00 +0530

    Fix-408 - Using Materlistic Alerts instead of document alert for notifying users

--- src/js/options.js
+++ src/js/options.js
@@ -15,11 +15,12 @@ var customBackground = document.getElementById("customBackground");
 var checkLogin = localStorage.getItem("checkLogin");
 var getVoice = document.getElementById("getVoice");
 var restore = document.getElementById("restore");
+var alerts = document.getElementById("alertback");
 var voice;
 
 
 window.onload = () => {
-    if(checkLogin === "true") {
+    if (checkLogin === "true") {
         customBackground.style.display = "block";
     }
     else {
@@ -28,7 +29,7 @@ window.onload = () => {
     }
 };
 
-if(micAccess) {
+if (micAccess) {
     micAccess.addEventListener("click", () => {
         navigator.webkitGetUserMedia({
             audio: true
@@ -39,75 +40,84 @@ if(micAccess) {
         });
     });
 }
-if(preDefThemes) {
-preDefThemes.addEventListener("click",(e) => {
-    if(e.target!==e.currentTarget){
-        theme= e.target.id;
-        console.log(theme);
-    }
+if (preDefThemes) {
+    preDefThemes.addEventListener("click", (e) => {
+        if (e.target !== e.currentTarget) {
+            theme = e.target.id;
+            console.log(theme);
+        }
 
-    e.stopPropagation;
+        e.stopPropagation;
     });
 }
 
-if(msgPaneThemes) {
- msgPaneThemes.addEventListener("click",(e) => {
-     if(e.target!==e.currentTarget){
-         msgTheme= e.target.id;
-       console.log(msgTheme);
-    }
+if (msgPaneThemes) {
+    msgPaneThemes.addEventListener("click", (e) => {
+        if (e.target !== e.currentTarget) {
+            msgTheme = e.target.id;
+            console.log(msgTheme);
+        }
 
-      e.stopPropagation;
-      });
-  }
-
-if(getVoice) {
-  getVoice.addEventListener("click",(e) => {
-     if(e.target!==e.currentTarget){
-         voice= e.target.id;
-    }
-
-     e.stopPropagation;
-     });
+        e.stopPropagation;
+    });
 }
 
-if(submit1) {
-			submit1.addEventListener("click",()=>{
-			localStorage.setItem("theme",theme);
-			alert("success");
-      });
+if (getVoice) {
+    getVoice.addEventListener("click", (e) => {
+        if (e.target !== e.currentTarget) {
+            voice = e.target.id;
+        }
+
+        e.stopPropagation;
+    });
 }
 
-if(submit2) {
-			submit2.addEventListener("click",()=>{
-			localStorage.setItem("msgTheme",msgTheme);
-			alert("success");
-      });
+if (submit1) {
+    submit1.addEventListener("click", () => {
+        localStorage.setItem("theme", theme);
+        document.getElementById("alerts").innerHTML = "<strong>Success</strong> Successfully, updated the theme color ";
+        document.getElementById("alertback").style.display = "block";
+    });
 }
-if(submit3) {
- 			submit3.addEventListener("click",()=>{
- 			localStorage.setItem("voice",voice);
- 			alert("success");
-      });
+
+if (submit2) {
+    submit2.addEventListener("click", () => {
+        localStorage.setItem("msgTheme", msgTheme);
+        document.getElementById("alerts").innerHTML = "<strong>Success</strong> Successfully, updated the message pane color ";
+        document.getElementById("alertback").style.display = "block";
+    });
 }
-if(backgroundChange) {
+if (submit3) {
+    submit3.addEventListener("click", () => {
+        localStorage.setItem("voice", voice);
+        document.getElementById("alerts").innerHTML = "<strong>Success</strong>,Changed the Voice Output";
+        document.getElementById("alertback").style.display = "block";
+    });
+}
+if (backgroundChange) {
     backgroundChange.addEventListener("submit", (e) => {
         e.preventDefault();
         theValue = backUrl.value;
         if (!theValue) {
-            alert("Error: No value specified");
+            document.getElementById("alerts").innerHTML = "<strong>Error</strong>  ";
+            document.getElementById("alertback").style.display = "block";
         } else {
             localStorage.setItem("theValue", theValue);
-            alert("Successfully stored");
+            document.getElementById("alerts").innerHTML = "<strong>Success</strong>,Stored  ";
+            document.getElementById("alertback").style.display = "block";
         }
     });
 }
 
-restore.addEventListener("click", ()=>{
-    if(confirm("This will remove your customized settings. This action is irreversible.")){
+restore.addEventListener("click", () => {
+    if (confirm("This will remove your customized settings. This action is irreversible.")) {
         userItems = ["theValue", "voice", "msgTheme", "theme"];
         userItems.forEach(item => {
             localStorage.removeItem(item);
         });
     }
+});
+
+alerts.addEventListener("click", () => {
+    alerts.style.display = "none";
 });
