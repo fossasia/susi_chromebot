f45b15741dca53b208c50d9512d9b724ce122ce4
Samagra Gupta <32234926+samagragupta@users.noreply.github.com>
2018-12-04 17:19:38 +0530

    Remember voice settings (#443)

--- src/script.js
+++ src/script.js
@@ -887,6 +887,12 @@ let check = () => {
     $("#scrollIcon").toggleClass("scroll-dark");
 };
 
+chrome.storage.sync.get("speakcheck", (obj) => {
+  if(obj.speakcheck === false){
+    document.getElementById("speak").click();
+  }
+});
+
 let changeSpeak = () => {
     shouldSpeak = !shouldSpeak;
     var SpeakIcon = document.getElementById("speak-icon");
@@ -896,6 +902,9 @@ let changeSpeak = () => {
         SpeakIcon.innerText = "volume_up";
     }
     console.log("Should be speaking? " + shouldSpeak);
+    chrome.storage.sync.set({
+        "speakcheck": shouldSpeak
+    }, () => {});
 };
 
 scrollIconTopElement.addEventListener("click", (e) => {
