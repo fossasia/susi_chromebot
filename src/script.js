ad28574a62ab916e3e1e1cccc7249fd9de0672e5
plxity <purutaneja.com@gmail.com>
2018-12-21 19:44:52 +0530

    padding added

--- src/script.js
+++ src/script.js
@@ -746,10 +746,10 @@ let toggleStartStop = () => {
             }
         },
         () => {
-            var messageN = '<div style="padding-left: 8px;">Please Enable Mic by setting option(Note: If you have blocked the mic before you have to remove it from chrome settings and then enable from extension)</div>';
+           
             $("body").overhang({
                 type:"error",
-                message: messageN,
+                message: '<div style="padding-left: 8px">Please Enable Mic by setting option(Note: If you have blocked the mic before you have to remove it from chrome settings and then enable from extension)</div>',
                 duration: 3,
                 html: true,
             });
