3a679f4b96d886b2cb06c7f269e1c2f8ea5a0e6a
Shubham Gupta <shubhamg@outlook.com>
2019-01-24 01:27:07 +0530

    Add Search Message Functionality #405

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
       options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
           indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: false,
           quotmark: true, regexp: true, undef: true, unused: true, trailing: true,
-          maxparams: 3, maxdepth: 4, maxstatements: 70, jquery: true, browser: true, devel: true, moz:true, strict: false,
+          maxparams: 6, maxdepth: 4, maxstatements: 70, jquery: true, browser: true, devel: true, moz:true, strict: false,
           esversion: 6},
       all: [
           'Gruntfile.js',
